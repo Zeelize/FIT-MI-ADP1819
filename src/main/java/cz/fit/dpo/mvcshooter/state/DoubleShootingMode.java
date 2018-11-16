3a768d604a0b5a20bc189a2e6d911ae6ff46bb61
Zeel <mraz.vojta@gmail.com>
2018-11-17 00:42:52 +0100

    Fix angle with double shoot

--- src/main/java/cz/fit/dpo/mvcshooter/state/DoubleShootingMode.java
+++ src/main/java/cz/fit/dpo/mvcshooter/state/DoubleShootingMode.java
@@ -5,14 +5,11 @@ import cz.fit.dpo.mvcshooter.model.entity.Cannon;
 public class DoubleShootingMode implements IShootingMode {
     @Override
     public void shoot(Cannon cannon) {
+        cannon.setAngle(cannon.getAngle() - 10);
         cannon.primitiveShoot();
-        cannon.aimDown();
-        cannon.aimDown();
-        cannon.aimDown();
-        cannon.aimDown();
-        cannon.aimDown();
-        cannon.aimDown();
+        cannon.setAngle(cannon.getAngle() + 20);
         cannon.primitiveShoot();
+        cannon.setAngle(cannon.getAngle() - 10);
     }
 
     @Override
--- src/main/java/cz/fit/dpo/mvcshooter/strategy/RandomMovementStrategy.java
+++ src/main/java/cz/fit/dpo/mvcshooter/strategy/RandomMovementStrategy.java
@@ -7,13 +7,15 @@ public class RandomMovementStrategy implements IMovementStrategy {
 
     @Override
     public int nextPosX(int initX, float initVel, float angle, long lifetime) {
+        // todo fix random movement
         double radians = Math.toRadians(angle);
-        return (rnd.nextInt(50) - 25) * (int) (initX + (initVel) * lifetime * Math.cos(radians));
+        return (int) (initX + (initVel) * lifetime * Math.cos(radians));
     }
 
     @Override
     public int nextPosY(int initY, float initVel, float angle, long lifetime) {
+        // todo fix random
         double radians = Math.toRadians(angle);
-        return (rnd.nextInt(50) - 25) * (int) ((initY + (initVel) * lifetime * Math.sin(radians)) + 0.01 * (lifetime * lifetime));
+        return rnd.nextInt(50) - 20 + (int) ((initY + (initVel) * lifetime * Math.sin(radians)) + 0.01 * (lifetime * lifetime));
     }
 }
